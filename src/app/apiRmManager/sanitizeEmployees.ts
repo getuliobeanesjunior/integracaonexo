81b527f26badb0a5c8782a425d82d30e56c0d183
jorg3ribeiro <jorge_ribeiro@live.com>
2023-04-11 18:54:52 -0300

    Retirando a data

--- src/app/apiRmManager/sanitizeEmployees.ts
+++ src/app/apiRmManager/sanitizeEmployees.ts
@@ -1,8 +1,4 @@
 import IEmployess from "../Interfaces/IEmployess";
-import dayjs from "dayjs";
-import utc from 'dayjs/plugin/utc'
-
-dayjs.extend(utc)
 
 export default ( listEmployess:Array<any> ): Array<IEmployess> =>{
     
@@ -18,12 +14,12 @@ export default ( listEmployess:Array<any> ): Array<IEmployess> =>{
             CodigoSetor:employee["CODIGOSETOR"] ? employee["CODIGOSETOR"][0] : null,
             CodigoCargo:employee["CODIGOCARGO"] ? employee["CODIGOCARGO"][0] : null,
             CodigoClassificador:null,
-            DataAdmissao:employee["DATAADMISSAO"] ? dayjs.utc(employee["DATAADMISSAO"][0]).format('YYYY-MM-DD') : null,
+            DataAdmissao:employee["DATAADMISSAO"] ? employee["DATAADMISSAO"][0] : null,
             CodigoUsuario:null,
             Ativo: employee["ATIVO"][0] === 1,
             CodigoTipoFuncionario:null,
-            DataNascimento:employee["DATANASCIMENTO"] ? dayjs.utc(employee["DATANASCIMENTO"][0]).format('YYYY-MM-DD') : null,
-            DataDemissao:employee["DATADEMISSAO"] ? dayjs(employee["DATADEMISSAO"][0]).format('YYYY-MM-DD') : null,
+            DataNascimento:employee["DATANASCIMENTO"] ? employee["DATANASCIMENTO"][0] : null,
+            DataDemissao:employee["DATADEMISSAO"] ? employee["DATADEMISSAO"][0] : null,
             CarteiraDeTrabalho:employee["CARTEIRADETRABALHO"] ? employee["CARTEIRADETRABALHO"][0] : null,
             NumeroDeSerieDaCarteiraDeTrabalho:employee["NUMERODESERIE"] ? employee["NUMERODESERIE"][0] : null,
             Sexo:employee["SEXO"] ? employee["SEXO"][0] : null,
@@ -45,8 +41,8 @@ export default ( listEmployess:Array<any> ): Array<IEmployess> =>{
             UfRG:employee["UFRG"] ? employee["UFRG"][0] : null,
             Uf:employee["UF"] ? employee["UF"][0] : null,
             NomeMae:null,
-            DataDeEmissao:employee["EMISSAODACARTEIRADETRABALHO"] ? dayjs(employee["EMISSAODACARTEIRADETRABALHO"][0]).format('YYYY-MM-DD') : null,
-            DataEmissaoRG:employee["DATAEMISSAORG"] ? dayjs(employee["DATAEMISSAORG"][0]).format('YYYY-MM-DD') : null,
+            DataDeEmissao:employee["EMISSAODACARTEIRADETRABALHO"] ? employee["EMISSAODACARTEIRADETRABALHO"][0] : null,
+            DataEmissaoRG:employee["DATAEMISSAORG"] ? employee["DATAEMISSAORG"][0] : null,
             OrgaoEmissorRG:employee["ORGAOEMISSORRG"] ? employee["ORGAOEMISSORRG"][0] : null,
             PIS:employee["PISPASEP"] ? employee["PISPASEP"][0] : null,
             Salario:employee["SALARIO"] ? parseFloat(employee["SALARIO"][0]) : 0,
@@ -57,16 +53,16 @@ export default ( listEmployess:Array<any> ): Array<IEmployess> =>{
             Aposentado:false,
             SituacaoBeneficiario:null,
             CodigoRaca:null,
-            DataInicioHistorico: employee["DATAINICIOHISTORICO"] ? dayjs(employee["DATAINICIOHISTORICO"][0]).format('YYYY-MM-DD') : null,
-            DataFinalHistorico: employee["DATAFINALHISTORICO"] ? dayjs(employee["DATAFINALHISTORICO"][0]).format('YYYY-MM-DD') : null,
-            DataInicioCargo: employee["DATAINICIOHISTORICOCARGO"] ? dayjs(employee["DATAINICIOHISTORICOCARGO"][0]).format('YYYY-MM-DD') : null,
-            DataFinalCargo: employee["DATAFINALHISTORICOCARGO"] ? dayjs(employee["DATAFINALHISTORICOCARGO"][0]).format('YYYY-MM-DD') : null,
-            DataInicioSetor: employee["DATAINICIOHISTORICOSETOR"] ? dayjs(employee["DATAINICIOHISTORICOSETOR"][0]).format('YYYY-MM-DD') : null,
-            DataFinalSetor: employee["DATAFINALHISTORICOSETOR"] ? dayjs(employee["DATAFINALHISTORICOSETOR"][0]).format('YYYY-MM-DD') : null,
-            DataInicioLotacao: employee["DATAINICIOHISTORICOLOTACAO"] ? dayjs(employee["DATAINICIOHISTORICOLOTACAO"][0]).format('YYYY-MM-DD') : null,
-            DataFinalLotacao: employee["DATAFINALHISTORICOLOTACAO"] ? dayjs(employee["DATAFINALHISTORICOLOTACAO"][0]).format('YYYY-MM-DD') : null,
-            DataInicioTurno: employee["DATAINICIOHISTORICOTURNO"] ? dayjs(employee["DATAINICIOHISTORICOTURNO"][0]).format('YYYY-MM-DD') : null,
-            DataFinalTurno: employee["DATAFINALHISTORICOTURNO"] ?  dayjs(employee["DATAFINALHISTORICOTURNO"] [0]).format('YYYY-MM-DD') : null
+            DataInicioHistorico: employee["DATAINICIOHISTORICO"] ? employee["DATAINICIOHISTORICO"][0] : null,
+            DataFinalHistorico: employee["DATAFINALHISTORICO"] ? employee["DATAFINALHISTORICO"][0] : null,
+            DataInicioCargo: employee["DATAINICIOHISTORICOCARGO"] ? employee["DATAINICIOHISTORICOCARGO"][0] : null,
+            DataFinalCargo: employee["DATAFINALHISTORICOCARGO"] ? employee["DATAFINALHISTORICOCARGO"][0] : null,
+            DataInicioSetor: employee["DATAINICIOHISTORICOSETOR"] ? employee["DATAINICIOHISTORICOSETOR"][0] : null,
+            DataFinalSetor: employee["DATAFINALHISTORICOSETOR"] ? employee["DATAFINALHISTORICOSETOR"][0] : null,
+            DataInicioLotacao: employee["DATAINICIOHISTORICOLOTACAO"] ? employee["DATAINICIOHISTORICOLOTACAO"][0] : null,
+            DataFinalLotacao: employee["DATAFINALHISTORICOLOTACAO"] ? employee["DATAFINALHISTORICOLOTACAO"][0] : null,
+            DataInicioTurno: employee["DATAINICIOHISTORICOTURNO"] ? employee["DATAINICIOHISTORICOTURNO"][0] : null,
+            DataFinalTurno: employee["DATAFINALHISTORICOTURNO"] ?  employee["DATAFINALHISTORICOTURNO"] [0] : null
         })
     }
 
