e800ae5f88a903617bd91ff65c962fdc12ca6ba3
jorg3ribeiro <jorge_ribeiro@live.com>
2023-04-12 12:22:58 -0300

    Alteração de cargo

--- src/app/apiRmManager/sanitizeOffice.ts
+++ src/app/apiRmManager/sanitizeOffice.ts
@@ -6,7 +6,7 @@ export default ( ListOffices:Array<any> ): Array<IOffice> =>{
 
     for(const office of ListOffices){
         offices.push({
-            Codigo:office["IDENTIFICADOR"] ? office["IDENTIFICADOR"][0] : null,
+            Codigo:office["CODIGO"] ? office["CODIGO"][0] : null,
             Nome:office["NOME"] ? office["NOME"][0] : null,
             Periodicidade:null,
             CBO:office["CBO"] ? office["CBO"][0] : null,
@@ -14,7 +14,7 @@ export default ( ListOffices:Array<any> ): Array<IOffice> =>{
             Ativo:office["ATIVO"] && office["ATIVO"][0] === '1',
             Descricao:office["CODIGO"] ? office["CODIGO"][0] : null,
             CodigoTipoAtividadeProfissional:null
-         })
+        })
     }
 
     return offices;
